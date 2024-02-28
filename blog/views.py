5ab1e491ca714703594164ad3da58df09cbd2209
Кирилл <rbhbkk-2000@mail.ru>
2024-02-28 22:17:19 +0500

    Creating presentation templates

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
 
 
 def post_list(request):
     posts = Post.published.all()
     return render(request,
-                  'blog/post/lost.html',
+                  'blog/post/list.html',
                   {'posts': posts})
 
 
